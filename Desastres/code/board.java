6f37c7f9d1b0652851cb085e7b6c4b99672c991f
Alejandro <alespinosap@gmail.com>
2022-03-05 12:28:51 +0100

    distribucion curro

--- Desastres/code/board.java
+++ Desastres/code/board.java
@@ -13,8 +13,10 @@ public class board {
 
     public static Grupos grupos;
     public static Centros centros;
-    public static ArrayList<Integer> helicopter; //mirar
-    
+    public static ArrayList<Integer> helicopter; //id del centro al que pertenecen
+
+    public static ArrayList<ArrayList<Integer>> estado;
+
     /**
     * Matriz donde precalcularemos las distancias de los centros a los grupos
     */
@@ -25,6 +27,8 @@ public class board {
     */
     public static ArrayList<ArrayList<Double>> distancia_grupos_grupos;
 
+
+    // 1. Creadora
     /**
      * Constructora por defecto
      */
@@ -35,9 +39,50 @@ public class board {
         grupos = g;
         centros = c;    
     }
-    
+
+    // 2. Gen estado inicial
     public void gen_estado_inicial(){
-    
-    
+
     }
+
+    // 3. Operadores
+    public void swap_grupos(){
+
+    }
+
+    public void reasignar_grupo(){
+
+    }
+
+    // 3. Calc distancia
+    public double calc_distancia(int x1, int y1, int x2, int y2){
+       return 1;
+    }
+
+    // 4. getters distancias
+    public double get_distancia(int id1, int id2, int select){
+        if(select == 0); //centro a grupos
+        else ; //grupos a grupos
+
+        return 1;
+    }
+
+    // 5. Precalcular distancias c_g
+    public ArrayList<ArrayList<Double>> precalc_dist_c_g(){
+        ArrayList<ArrayList<Double>> aux = new ArrayList<ArrayList<Double>>();
+        return aux;
+    }
+
+    // 6. Precalcular distancias g_g
+    public ArrayList<ArrayList<Double>> precalc_dist_g_g(){
+        ArrayList<ArrayList<Double>> aux = new ArrayList<ArrayList<Double>>();
+        return aux;
+    }
+
+    // 7. initialize, quiza no se usa
+    public void initialize(){
+
+    }
+
+    //hacer un main
 }
--- Desastres/code/estado.java
+++ Desastres/code/estado.java
@@ -8,13 +8,5 @@ import java.util.*;
  
 public class estado {
 
-    public static ArrayList<ArrayList<Integer>> estado;
-    
-    public estado(){
-    }
-
-    public estado(ArrayList<ArrayList<Integer>> estadoini){
-        estado = estadoini;
-    }
     
 }
