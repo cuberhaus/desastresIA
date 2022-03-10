bfc894e97e758b510f43399696d8771c79c87107
Alejandro <alespinosap@gmail.com>
2022-03-10 18:19:30 +0100

    comentarios

--- Desastres/src/Desastres/DesastresHeuristicFunction1.java
+++ Desastres/src/Desastres/DesastresHeuristicFunction1.java
@@ -5,8 +5,12 @@ import aima.search.framework.HeuristicFunction;
 
 public class DesastresHeuristicFunction1 implements HeuristicFunction{
 
+    //minimizar máximo de las distancias de todos los helicópteros
+    //1 helicóptero puede como mucho llevar 15 personas, 10 mins cd entre viajes
     public double getHeuristicValue(Object estado) {
-        return 0;
+        double heuristic = 0;
+
+       return heuristic;
     }
 
 }
