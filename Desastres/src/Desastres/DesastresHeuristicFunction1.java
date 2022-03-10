ec33e3d196d315408fe17b386148ac03b7803903
Alejandro <alespinosap@gmail.com>
2022-03-10 18:06:41 +0100

    Aima movido para que el proyecto funcione, classe funcion heuristica1

---
+++ Desastres/src/Desastres/DesastresHeuristicFunction1.java
@@ -0,0 +1,12 @@
+package Desastres;
+
+import aima.search.framework.HeuristicFunction;
+
+
+public class DesastresHeuristicFunction1 implements HeuristicFunction{
+
+    public double getHeuristicValue(Object estado) {
+        return 0;
+    }
+
+}
