f413e5f0be4d50b433da4ba9625582c1d8a10a89
Pol Casacuberta <polcg10@gmail.com>
2022-03-14 20:36:59 +0100

    heuristic 2 should be working properly now

--- Desastres/src/Desastres/DesastresHeuristicFunction2.java
+++ Desastres/src/Desastres/DesastresHeuristicFunction2.java
@@ -13,12 +13,12 @@ public class DesastresHeuristicFunction2 {
         double heuristic = 0;
         board area = (board)estado;
         ArrayList<LinkedList<Integer>> estadoact = area.getestado();
-        double tmax = -1;
+//        double tmax = -1;
         double max_dist = -1;
         for(int i = 0; i < estadoact.size(); ++i){
             //Capacitat actual per l'helicópter actual en el viatje que "esta realitzant"
             int capacitatact = 0;
-            double tiempoact = 0;
+//            double tiempoact = 0;
             int centroact = area.getcentro(i);
             int lastgroup = -1;
             int distact = 0;
@@ -30,41 +30,41 @@ public class DesastresHeuristicFunction2 {
 
                     //sales del centro
                     if(lastgroup == -1){
-                        tiempoact += (area.get_distancia(centroact, estadoact.get(i).get(j), board.select_distance.CENTER_TO_GROUP))/1.66667;
+//                        tiempoact += (area.get_distancia(centroact, estadoact.get(i).get(j), board.select_distance.CENTER_TO_GROUP))/1.66667;
                         distact += area.get_distancia(centroact, estadoact.get(i).get(j), board.select_distance.CENTER_TO_GROUP);
-
-                        int timeperpeople = 1;
-                        if(g.getPrioridad() == 1) timeperpeople = 2;
-
-                        tiempoact += (g.getNPersonas() *timeperpeople);
+//
+//                        int timeperpeople = 1;
+//                        if(g.getPrioridad() == 1) timeperpeople = 2;
+//
+//                        tiempoact += (g.getNPersonas() *timeperpeople);
                         lastgroup = estadoact.get(i).get(j);
 
                     } else{
                         //sales de un grupo
-                        tiempoact += (area.get_distancia(lastgroup, estadoact.get(i).get(j), board.select_distance.GROUP_TO_GROUP))/1.66667;
+//                        tiempoact += (area.get_distancia(lastgroup, estadoact.get(i).get(j), board.select_distance.GROUP_TO_GROUP))/1.66667;
                         distact += area.get_distancia(lastgroup, estadoact.get(i).get(j), board.select_distance.GROUP_TO_GROUP);
 
-                        int timeperpeople = 1;
-                        if(g.getPrioridad() == 1) timeperpeople = 2;
-
-                        tiempoact += (g.getNPersonas() *timeperpeople);
+//                        int timeperpeople = 1;
+//                        if(g.getPrioridad() == 1) timeperpeople = 2;
+//
+//                        tiempoact += (g.getNPersonas() *timeperpeople);
                         lastgroup = estadoact.get(i).get(j);
                     }
 
                 } else{
                     //viaje "lleno"
                     capacitatact = 0;
-                    tiempoact += (area.get_distancia(centroact, lastgroup, board.select_distance.CENTER_TO_GROUP))/1.66667;
+//                    tiempoact += (area.get_distancia(centroact, lastgroup, board.select_distance.CENTER_TO_GROUP))/1.66667;
                     distact += area.get_distancia(centroact, lastgroup, board.select_distance.CENTER_TO_GROUP);
                     //10 min cooldown
-                    tiempoact += 10;
+//                    tiempoact += 10;
 
                     lastgroup = -1;
 
                 }
             }
-            if(max_dist == -1) tmax = tiempoact;
-            else if(tmax < tiempoact) tmax = tiempoact;
+            if(max_dist == -1) max_dist = distact;
+            else if(max_dist < distact) max_dist = distact;
         }
 
         return heuristic;
