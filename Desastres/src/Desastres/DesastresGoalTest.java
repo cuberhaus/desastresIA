ae5d544d77ed2274e15a35ec30726362fd10c6f0
Alejandro <alespinosap@gmail.com>
2022-03-14 21:39:16 +0100

    bugs

--- Desastres/src/Desastres/DesastresGoalTest.java
+++ Desastres/src/Desastres/DesastresGoalTest.java
@@ -1,7 +1,6 @@
 package Desastres;
 
 import aima.search.framework.GoalTest;
-import aima.search.*;
 
 public class DesastresGoalTest  implements GoalTest {
     public boolean isGoalState(Object aState) {
