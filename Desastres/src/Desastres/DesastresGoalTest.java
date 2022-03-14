349177593a862aef60878b875479a6466bac1166
Alejandro <alespinosap@gmail.com>
2022-03-14 21:36:40 +0100

    clase estado final hecha

---
+++ Desastres/src/Desastres/DesastresGoalTest.java
@@ -0,0 +1,10 @@
+package Desastres;
+
+import aima.search.framework.GoalTest;
+import aima.search.*;
+
+public class DesastresGoalTest  implements GoalTest {
+    public boolean isGoalState(Object aState) {
+        return(false);
+    }
+}
