13637ee4c706dadeda8c264e605c833ac6ff05af
Alejandro <alespinosap@gmail.com>
2022-03-31 17:31:11 +0200

    heurisitico3 priorizar grupos heridos doned

--- Desastres/src/main.java
+++ Desastres/src/main.java
@@ -40,7 +40,7 @@ public class main {
         int algorithm = 0;
         int successorfunc = 4;
         int gensolini = 2;
-        int heuristicfunc = 1;
+        int heuristicfunc = 3;
         //selector for successor
 
         aima.search.framework.SuccessorFunction SF = null;
@@ -63,7 +63,7 @@ public class main {
         int seed = 1000;
         int ncentros = 5;
         int ngrupos = 100;
-        int nhelicopters = 10;
+        int nhelicopters = 1;
 
 
         if (args.length == 1) {
@@ -226,24 +226,28 @@ public class main {
 
     private static double gettime(Object estat) {
         double heuristic = 0;
-        ArrayList<LinkedList<Integer>> estadoact = ((estado)estat).getvec();
+        ArrayList<LinkedList<Integer>> estadoact = ((estado) estat).getvec();
 
+        //el que mas tarda
+        double tmax = -1;
         //suma todos
         double ttotal = 0;
 
-        for(int i = 0; i < estadoact.size(); ++i){
+        double prioritytime = 0;
+
+        ArrayList<Double> tiemposheli = new ArrayList<>();
+        ArrayList<Integer> ngrupos = new ArrayList<>();
+        for (int i = 0; i < estadoact.size(); ++i) {
             //Capacitat actual per l'helicópter actual en el viatje que "esta realitzant"
             int capacitatact = 0;
             double tiempoact = 0;
             int centroact = board.getcentro(i);
-//            System.out.println("centerNew" + centroact);
-//            System.out.println("centerOld" + board.getcentro(i));
             int lastgroup = -1;
             int ngrups = 0;
-            for(int j = 0; j < estadoact.get(i).size(); ++j){
+            boolean priorittravel = false;
+            for (int j = 0; j < estadoact.get(i).size(); ++j) {
                 Grupo g = board.getgrupo(estadoact.get(i).get(j));
-                //para mirar si hay que volver al centro (ultimo grupo a rescatar)
-                if(j != estadoact.get(i).size()-1) {
+                if (j != estadoact.get(i).size() - 1) {
                     if (capacitatact + g.getNPersonas() <= 15 && ngrups < 3) {
                         //Aún cabe gente en el helicóptero para este viaje
                         capacitatact += g.getNPersonas();
@@ -251,21 +255,31 @@ public class main {
                         //sales del centro
                         if (lastgroup == -1) {
                             tiempoact += (board.get_distancia(centroact, estadoact.get(i).get(j), board.select_distance.CENTER_TO_GROUP)) / 1.66667;
+                            ttotal += (board.get_distancia(centroact, estadoact.get(i).get(j), board.select_distance.CENTER_TO_GROUP)) / 1.66667;
                             //System.out.println(board.get_distancia(centroact, estadoact.get(i).get(j), board.select_distance.CENTER_TO_GROUP));
                             int timeperpeople = 1;
-                            if (g.getPrioridad() == 1) timeperpeople = 2;
+                            if (g.getPrioridad() == 1) {
+                                timeperpeople = 2;
+                                priorittravel = true;
+                            }
 
                             tiempoact += (g.getNPersonas() * timeperpeople);
+                            ttotal += (g.getNPersonas() * timeperpeople);
                             lastgroup = estadoact.get(i).get(j);
 
                         } else {
                             //sales de un grupo
                             tiempoact += (board.get_distancia(lastgroup, estadoact.get(i).get(j), board.select_distance.GROUP_TO_GROUP)) / 1.66667;
+                            ttotal += (board.get_distancia(lastgroup, estadoact.get(i).get(j), board.select_distance.GROUP_TO_GROUP)) / 1.66667;
 
                             int timeperpeople = 1;
-                            if (g.getPrioridad() == 1) timeperpeople = 2;
+                            if (g.getPrioridad() == 1) {
+                                timeperpeople = 2;
+                                priorittravel = true;
+                            }
 
                             tiempoact += (g.getNPersonas() * timeperpeople);
+                            ttotal += (g.getNPersonas() * timeperpeople);
                             lastgroup = estadoact.get(i).get(j);
                         }
 
@@ -273,72 +287,152 @@ public class main {
                         //viaje "lleno", ya sea por limite de personas o por numero de grupos
                         capacitatact = 0;
                         tiempoact += (board.get_distancia(centroact, lastgroup, board.select_distance.CENTER_TO_GROUP)) / 1.66667;
+                        ttotal += (board.get_distancia(centroact, lastgroup, board.select_distance.CENTER_TO_GROUP)) / 1.66667;
+
+                        if(priorittravel) {
+                            prioritytime = ttotal;
+                            priorittravel = false;
+                        }
                         //10 min cooldown
                         tiempoact += 10;
+                        ttotal += 10;
 
 
                         tiempoact += (board.get_distancia(centroact, estadoact.get(i).get(j), board.select_distance.CENTER_TO_GROUP)) / 1.66667;
+                        ttotal += (board.get_distancia(centroact, estadoact.get(i).get(j), board.select_distance.CENTER_TO_GROUP)) / 1.66667;
                         int timeperpeople = 1;
-                        if (g.getPrioridad() == 1) timeperpeople = 2;
+                        if (g.getPrioridad() == 1) {
+                            timeperpeople = 2;
+                            priorittravel = true;
+                        }
 
                         tiempoact += (g.getNPersonas() * timeperpeople);
+                        ttotal += (g.getNPersonas() * timeperpeople);
                         lastgroup = estadoact.get(i).get(j);
                         ngrups = 1;
                     }
-                } else if(capacitatact + g.getNPersonas() <= 15 && ngrups < 3){
+                } else if (capacitatact + g.getNPersonas() <= 15 && ngrups < 3) {
                     //recoges ultimo grupo y vuelves
                     capacitatact += g.getNPersonas();
                     ++ngrups;
                     //sales del centro
                     if (lastgroup == -1) {
                         tiempoact += (board.get_distancia(centroact, estadoact.get(i).get(j), board.select_distance.CENTER_TO_GROUP)) / 1.66667;
+                        ttotal += (board.get_distancia(centroact, estadoact.get(i).get(j), board.select_distance.CENTER_TO_GROUP)) / 1.66667;
                         //System.out.println(board.get_distancia(centroact, estadoact.get(i).get(j), board.select_distance.CENTER_TO_GROUP));
                         int timeperpeople = 1;
-                        if (g.getPrioridad() == 1) timeperpeople = 2;
+                        if (g.getPrioridad() == 1) {
+                            timeperpeople = 2;
+                            priorittravel = true;
+                        }
 
                         tiempoact += (g.getNPersonas() * timeperpeople);
+                        ttotal += (g.getNPersonas() * timeperpeople);
                         lastgroup = estadoact.get(i).get(j);
 
                     } else {
                         //sales de un grupo
                         tiempoact += (board.get_distancia(lastgroup, estadoact.get(i).get(j), board.select_distance.GROUP_TO_GROUP)) / 1.66667;
+                        ttotal += (board.get_distancia(lastgroup, estadoact.get(i).get(j), board.select_distance.GROUP_TO_GROUP)) / 1.66667;
 
                         int timeperpeople = 1;
-                        if (g.getPrioridad() == 1) timeperpeople = 2;
+                        if (g.getPrioridad() == 1){
+                            timeperpeople = 2;
+                            priorittravel = true;
+                        }
 
                         tiempoact += (g.getNPersonas() * timeperpeople);
+                        ttotal += (g.getNPersonas() * timeperpeople);
                         lastgroup = estadoact.get(i).get(j);
                     }
                     capacitatact = 0;
                     tiempoact += (board.get_distancia(centroact, estadoact.get(i).get(j), board.select_distance.CENTER_TO_GROUP)) / 1.66667;
+                    ttotal += (board.get_distancia(centroact, estadoact.get(i).get(j), board.select_distance.CENTER_TO_GROUP)) / 1.66667;
+
+                    if(priorittravel) {
+                        prioritytime = ttotal;
+                        priorittravel = false;
+                    }
+
                     lastgroup = estadoact.get(i).get(j);
                     ngrups = 1;
 
-                } else{
+                } else {
                     //dejar grupo, wait, coger grupo, volver
                     capacitatact = 0;
                     tiempoact += (board.get_distancia(centroact, lastgroup, board.select_distance.CENTER_TO_GROUP)) / 1.66667;
+                    ttotal += (board.get_distancia(centroact, lastgroup, board.select_distance.CENTER_TO_GROUP)) / 1.66667;
                     //10 min cooldown
+
+                    if(priorittravel) {
+                        prioritytime = ttotal;
+                        priorittravel = false;
+                    }
+
                     tiempoact += 10;
 
-
                     tiempoact += (board.get_distancia(centroact, estadoact.get(i).get(j), board.select_distance.CENTER_TO_GROUP)) / 1.66667;
+                    ttotal += (board.get_distancia(centroact, estadoact.get(i).get(j), board.select_distance.CENTER_TO_GROUP)) / 1.66667;
                     int timeperpeople = 1;
-                    if (g.getPrioridad() == 1) timeperpeople = 2;
+                    if (g.getPrioridad() == 1) {
+                        timeperpeople = 2;
+                        priorittravel = true;
+                    }
 
                     tiempoact += (g.getNPersonas() * timeperpeople);
+                    ttotal += (g.getNPersonas() * timeperpeople);
                     lastgroup = estadoact.get(i).get(j);
                     ngrups = 1;
 
                     capacitatact = 0;
                     tiempoact += (board.get_distancia(centroact, estadoact.get(i).get(j), board.select_distance.CENTER_TO_GROUP)) / 1.66667;
+                    ttotal += (board.get_distancia(centroact, estadoact.get(i).get(j), board.select_distance.CENTER_TO_GROUP)) / 1.66667;
+
+                    if(priorittravel) {
+                        prioritytime = ttotal;
+                        priorittravel = false;
+                    }
                 }
             }
-            ttotal += tiempoact;
-            //System.out.println(tiempoact);
+            //tiempo del helicoptero que mas tarda
+            if (tmax == -1) tmax = tiempoact;
+            else if (tmax < tiempoact) tmax = tiempoact;
+
+            //suma de cuadrado de tiempos
+            //tmax += (Math.pow(tiempoact,2));
+
+            //tmax += tiempoact;
+
+            //opcion Sara
+            tiemposheli.add(tiempoact);
+            ngrupos.add(estadoact.get(i).size());
         }
 
 
+        double aux = 0.0;
+        double nhelisingrupo = 0.0;
+        /*
+        for(int i = 0; i < tiemposheli.size(); ++i){
+            if(tiemposheli.get(i) != 0) aux += (((tiemposheli.get(i)/ttotal))*(Math.log10((tiemposheli.get(i)/ttotal))));
+            else nhelisingrupo++;
+            //if(tiemposheli.get(i) != 0) aux += (tiemposheli.get(i)*(Math.log10((tiemposheli.get(i)/tmax))));
+        }
+        */
+
+        for (int i = 0; i < tiemposheli.size(); ++i) {
+            if (tiemposheli.get(i) != 0) aux += tiemposheli.get(i) * (ngrupos.get(i) / (float) board.grupos.size());
+            else nhelisingrupo++;
+            //if(tiemposheli.get(i) != 0) aux += (tiemposheli.get(i)*(Math.log10((tiemposheli.get(i)/tmax))));
+        }
+
+        double ponderacion = (1 - ((ttotal / numhelicopters) / tmax));
+
+
+        //heuristic = (ttotal + aux * ponderacion) + prioritytime;
+        //System.out.println("tmax: " + tmax);
+        //System.out.println("aux: "+ aux);
+
+        System.out.println("Priority time: " + prioritytime);
         heuristic = ttotal;
         //System.out.println(heuristic);
         return heuristic;
