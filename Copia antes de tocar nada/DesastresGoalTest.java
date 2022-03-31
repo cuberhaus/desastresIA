e4c52c9f8d422f59cd08ece3e245bbe01709e41a
Pol Casacuberta <polcg10@gmail.com>
2022-03-31 23:04:38 +0200

    aima files moved to aima folder

--- Copia antes de tocar nada/DesastresGoalTest.java
+++
@@ -1,9 +0,0 @@
-package Desastres;
-
-import aima.search.framework.GoalTest;
-
-public class DesastresGoalTest  implements GoalTest {
-    public boolean isGoalState(Object aState) {
-        return(false);
-    }
-}
